bebde8380e03684224ba714ef81fdaec1cb1cfba
darkkwing <dargamarr@gmail.com>
2025-08-10 16:18:54 -0300

    pregunta 5 entrega

--- eventos.js
+++ eventos.js
@@ -1,8 +1,11 @@
 let saludar = "Hola!"
 
-function saludo() {
+function saludo(evento) {
+    evento.stopPropagation();
     alert(saludar)  
 };
+
+document.getElementById("btnSaludar").addEventListener("click", saludo);
 
 document.getElementById("holaDiv").addEventListener("click", function() {
     alert(saludar + " Soy el div");
